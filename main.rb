4fcd08455954ecd1c93b5f5861d9f481e14881b1
melinaDaendliker <melina.daendliker@hotmailc.om>
2023-10-16 09:25:21 +0200

    write prepend function

--- main.rb
+++ main.rb
@@ -13,22 +13,48 @@ class LinkedList
     cur = @head
     while cur.next_node != nil
       cur = cur.next_node
-      p cur.value
-      p cur.next_node
+      #p cur.value
+      #p cur.next_node
     end
     cur.next_node = end_node
+  end
+
+  def prepend(value)
+    cur = @head
+    1.times do
+      cur = cur.next_node
+      #p cur
+      if cur == nil
+        append(value)
+      else
+        #p cur.value
+        #p cur.next_node
+        start_node = Node.new(value)
+        start_node.next_node = cur
+        #p start_node
+        @head.next_node = start_node
+      end
+    end
   end
 end
 
 
 
 list = LinkedList.new
+p list
+list.prepend("start")
+p list
 list.append("last one")
 p list
 list.append("real last one")
 p list
 list.append(3)
 p list
+list.prepend("new start")
+p list
+list.prepend("first one")
+p list 
+
 
 
 
