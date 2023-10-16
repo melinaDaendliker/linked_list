460f396f357a91c869fe4f299a81b2c268984244
melinaDaendliker <melina.daendliker@hotmailc.om>
2023-10-16 08:32:28 +0200

    write append function

--- main.rb
+++ main.rb
@@ -2,14 +2,33 @@ require_relative 'lib/node.rb'
 
 class LinkedList
 
+  attr_accessor :head
+
+  def initialize(head = nil)
+    @head = Node.new
+  end
+  
+  def append(value)
+    end_node = Node.new(value)
+    cur = @head
+    while cur.next_node != nil
+      cur = cur.next_node
+      p cur.value
+      p cur.next_node
+    end
+    cur.next_node = end_node
+  end
 end
 
 
-test = Node.new
 
-p test
-
-p test2 = Node.new(2,3)
+list = LinkedList.new
+list.append("last one")
+p list
+list.append("real last one")
+p list
+list.append(3)
+p list
 
 
 
